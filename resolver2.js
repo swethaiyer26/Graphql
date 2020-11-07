433ce5f70dfddebd9822b6c98ae3d134146181e5
Swetha Janardhan <swethaiyer26@gmail.com>
2020-11-07 21:52:47 +0530

    Add files via upload

---
+++ resolver2.js
@@ -0,0 +1,36 @@
+const db = require('./db2')
+const Query = {
+   //resolver function for greeting
+   greeting:() => {
+      return "hello from  TutorialsPoint !!!"
+   },
+   
+   //resolver function for students returns list
+   students:() => db.students.list(),
+
+   //resolver function for studentbyId
+   studentById:(root,args,context,info) => {
+      //args will contain parameter passed in query
+      return db.students.get(args.id);
+   },
+   sayHello:(root,args,context,info) => `Hi ${args.name} GraphQL server says Hello to you!!`,
+
+   setFavouriteColor:(root,args) => {
+    return  "Your Fav Color is :"+args.color;
+ }
+}
+
+//for each single student object returned,resolver is invoked
+
+const Student = {
+   fullName:(root,args,context,info) => {
+      return root.firstName+":"+root.lastName
+   },
+   college:(root)=>{
+       return db.colleges.get(root.collegeId)
+   }
+
+}
+
+
+module.exports = {Query,Student}
